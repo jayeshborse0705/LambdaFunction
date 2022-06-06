b65388982691ba841978eac843affeba2090db3a
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-06-06 15:20:09 +0530

    [jayesh]  create class Person Details

---
+++ LambdaFunction/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaFunction
+{
+    internal class Person
+    {
+        public int SSN;
+        public string name; 
+        public string address;    
+        public int age;
+        public Person(int id, string name, string address, int age)
+        {
+            this.SSN=id;
+            this.name=name; 
+            this.address=address; 
+            this.age=age;   
+        }
+
+    }
+}
---
+++ LambdaFunction/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaFunction
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Person> PeopleList = new List<Person>();
+            PeopleList.Add(new Person(20, "hansraj", "munbai(MH)", 17));
+            PeopleList.Add(new Person(40, "Yogesh", "Pune(MH)", 21));
+            PeopleList.Add(new Person(60, "Rushikesh", "Valsad(GJ)", 61));
+            PeopleList.Add(new Person(51, "prashant", "Panji(Goa)", 71));
+            PeopleList.Add(new Person(40, "Rahul", "bhopal(MP)", 58));
+            
+        }
+    }
+}
